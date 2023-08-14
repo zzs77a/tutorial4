fe66d26c65aad4858ea0a59c4e72e19a5fc1b0ec
Amjed Tahir <amjedtahir@gmail.com>
2023-08-14 14:39:24 +1200

    added files

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalcTest {
+  Calc c = new Calc();
+
+    @Test
+    void testAddition() {
+        assertEquals(4, c.add(2,2));
+    }
+
+}
