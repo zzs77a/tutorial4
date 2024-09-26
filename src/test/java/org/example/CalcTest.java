a43af0e996a6203fdca89b8fc0cc4bdbd1b596b5
zzs77a <2317531163@qq.com>
2024-09-26 22:53:39 +0800

    add tesr

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 public class CalcTest {
   Calc c = new Calc();
 
+
     @Test
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+
+    assertEquals(2, c.subtract(4,2))
 
 }
